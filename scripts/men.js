37234b23342cac88f735bbe1804dd16e95067351
mdtabish803116 <mdtabish803116@gmail.com>
2022-04-07 01:00:20 +0530

    added modified files

---
+++ scripts/men.js
@@ -0,0 +1,220 @@
+
+var mensData = [
+       
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-EX4296/reebok_EX4296_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        price : 1170,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-EX3921/reebok_EX3921_1.jpg.plp",
+        name : "MEN'S REEBOK SWIM ARUBA FLIP SLIPPERS",
+        price : 1182,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-EX4160/reebok_EX4160_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING AUSTIN SHOES",
+        price : 1003,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FV8798/MEN_SWIM_SLIPPERS_FV8798_1.jpg.plp",
+        name : "MEN'S REEBOK SWIM AVENGER FLIP LP SLIPPERS",
+        price : 1006,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-EW5173/reebok_EW5173_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING TEMPO SHOES",
+        price :987 ,
+    },
+
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FV7329/reebok_FV7329_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING ASTRO BOOSTER SHOES",
+        price : 564,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-GD8567/MEN_TRAINING_PANTS_GD8567_1.jpg.plp",
+        name : "MEN'S TRAINING WORKOUT READY ELITAGE PANTS",
+        price :564 ,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-GQ8758/reebok_GQ8758_1.jpg.plp",
+        name : "MEN'S REEBOK TRAINING POLO T-SHIRTS TEE",
+        price : 342,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-GP7180/reebok_GP7180_1.jpg.plp",
+        name : "MEN'S REEBOK TRAINING WORKOUT TEE",
+        price : 342,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-EX3924/reebok_EX3924_1.jpg.plp",
+        name : "MEN'S REEBOK SUPER SOFT FLIP SLIPPERS",
+        price : 986,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FW0343/reebok_FW0343_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING RUN SHOES",
+        price : 786,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-EX4285/reebok_EX4285_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING GENESIS RUNNER SHOES",
+        price : 1356,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FV9519/MEN_SWIM_SLIPPERS_FV9519_1.jpg.plp",
+        name : "MEN'S REEBOK SWIM RUSSLE FLIP SLIPPERS",
+        price : 546,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FW0318/reebok_FW0318_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING TREND RUNNER SHOES",
+        price : 784,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-H00024/reebok_H00024_1.jpg.plp",
+        name : "UNISEX REEBOK RUNNING ZIG KINETICA II SHOES",
+        price : 598,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-GK9170/reebok_GK9170_1.jpg.plp",
+        name : "MEN'S REEBOK TRAINING BAS POLO TEE",
+        price : 453,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-GK9175/reebok_GK9175_1.jpg.plp",
+        name : "MEN'S REEBOK TRAINING FOUNDATION PANTS",
+        price : 999,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-GK9176/reebok_GK9176_1.jpg.plp",
+        name : "MEN'S REEBOK TRAINING FOUNDATION PANTS",
+        price : 456,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FW0318/reebok_FW0318_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING TREND SHOES",
+        price : 867,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-G57776/reebok_G57776_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING ZTAUR RUN SHOES",
+        price : 455,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FV9555/MEN_SWIM_SLIPPERS_FV9555_1.jpg.plp",
+        name : "MEN'S REEBOK SWIM ULTRA FLIP SLIPPERS",
+        price : 345,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-EX3994/reebok_EX3994_1.jpg.plp",
+        name : "MEN'S REEBOK SWIM FLEX CONNECT SANDALS",
+        price : 767,
+    },
+      
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-EX4287/reebok_EX4287_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING GENESIS RUNNER SHOE",
+        price : 320,
+    },
+     
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-GK9180/reebok_GK9180_1.jpg.plp",
+        name : "MEN'S REEBOK TRAINING RUNNER SHORTS",
+        price : 376,
+    },
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-HB5732/reebok_HB5732_1.jpg.plp",
+        name : "MEN REEBOK CLASSICS TRACKSUIT HOODIE",
+        price : 8004,
+    },
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-HB5733/reebok_HB5733_1.jpg.plp",
+        name : "MEN'S REEBOK CLASSIC TRACKSUIT GOLF HOODIE",
+        price : 776,
+    },
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FQ5364/reebok_FQ5364_1.jpg.plp",
+        name : "UNISEX REEBOK TRAINING ACTIVE BAG",
+        price : 478,
+    },
+    {
+        img_url :"https://content.shop4reebok.com/static/Product-FW0318/reebok_FW0318_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING TREND RUNNER SHOES",
+        price : 986,
+    },
+    
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FW0318/reebok_FW0318_1.jpg.plp",
+        name : "UNISEX REEBOK CLASSICS BASKETBALL SHOES",
+        price : 1008,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-G57705/reebok_G57705_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING RUN FUSION SHOES",
+        price : 1302,
+    },
+
+    {
+        img_url : "https://content.shop4reebok.com/static/Product-FW0344/reebok_FW0344_1.jpg.plp",
+        name : "MEN'S REEBOK RUNNING RUN FUSION SHOES",
+price : 1205,
+}
+]
+
+ displayData(mensData);
+
+function displayData(mensData){
+       mensData.forEach(function(menItem){
+          var menCart = document.createElement("div");
+            var img = document.createElement("img");
+              img.src = menItem.img_url;
+             var name = document.createElement("p");
+               name.textContent = menItem.name;
+              var price = document.createElement("p");
+                 price.textContent = `Rs.${menItem.price}`;
+            var cartBtn = document.createElement("button");
+                cartBtn.textContent = "Add to cart";
+
+            // cartBtn.addEventListener("click" , addToCart);
+            menCart.append(img , name , price , cartBtn);
+          document.getElementById("container").append(menCart);
+
+       })
+}
+
+//    var cartItems;
+//    if(localStorage.getItem("cartItems") === null){
+//        cartItems = [];
+//    }else {
+//        cartItems = JSON.parse(localStorage.getItem("cartItems"));
+//    }
+
+// function addToCart(){
+
+// }
+
